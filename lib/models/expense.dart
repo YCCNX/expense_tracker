48a26ca6cee7cb326899a62c68b55585c597bf29
YCCNX <yingtakoul@gmail.com>
2024-04-15 15:35:54 +0700

    Update infrastucture

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,10 +1,28 @@
 import 'package:uuid/uuid.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work, other }
 
+const categoryIcons = {
+  Category.food: Icons.dinner_dining,
+  Category.travel: Icons.flight,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+  Category.other: Icons.devices_other
+};
+
 class Expense {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
   Expense({
     required this.title,
     required this.amount,
@@ -12,9 +30,7 @@ class Expense {
     required this.category,
   }) : id = uuid.v4();
 
-  final String id;
-  final String title;
-  final double amount;
-  final DateTime date;
-  final Category category;
+  String get formetteDate {
+    return formatter.format(date);
+  }
 }
